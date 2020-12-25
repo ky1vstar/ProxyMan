f5a996f34dd780f80c8f7401c0e456f598718f20
Aleksandr Zhuhan <ky1vstar@ex.ua>
2020-12-25 16:17:34 +0200

    Initial project setup

---
+++ Proxyman/App/SceneDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  SceneDelegate.swift
+//  Proxyman
+//
+//  Created by KY1VSTAR on 25.12.2020.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        if let windowScene = scene as? UIWindowScene {
+            let contentView = ContentView()
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
+
